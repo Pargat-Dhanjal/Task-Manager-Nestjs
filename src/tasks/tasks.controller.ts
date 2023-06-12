a94a8a55141e0a6a4576a2b5db6c9703c40d51c6
Pargat-Dhanjal <pargat14@hotmail.com>
2023-06-12 15:44:16 +0530

    feat: filter tasks, rest api complete

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,14 +1,27 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
 import { createTaskDto } from './dto/create-task.dto';
+import { getTaskFilterDto } from './dto/get-tasks-filter.dto';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private taskService: TasksService) {}
 
   @Get()
-  getAllTasks(): Task[] {
+  getTasks(@Query() filterGto: getTaskFilterDto): Task[] {
+    if (Object.keys(filterGto).length) {
+      return this.taskService.getTaskFilter(filterGto);
+    }
     return this.taskService.getAllTask();
   }
 
@@ -20,5 +33,19 @@ export class TasksController {
   @Get('/:id')
   getTaskById(@Param('id') id: string): Task {
     return this.taskService.getTaskById(id);
+  }
+
+  @Delete('/:id')
+  deleteTaskById(@Param('id') id: string): Task {
+    return this.taskService.deleteTaskById(id);
+  }
+
+  @Patch('/:id/:type')
+  patchTaskById(
+    @Param('id') id: string,
+    @Param('type') type: string,
+    @Body('value') value: string,
+  ): Task {
+    return this.taskService.patchTaskById(id, type, value);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import * as uuid from 'uuid';
 import { createTaskDto } from './dto/create-task.dto';
+import { getTaskFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
@@ -9,6 +10,21 @@ export class TasksService {
 
   getAllTask(): Task[] {
     return this.tasks;
+  }
+
+  getTaskFilter(filterDto: getTaskFilterDto): Task[] {
+    const { status, search } = filterDto;
+    let tasks = this.getAllTask();
+    if (status) {
+      tasks = tasks.filter((task) => task.status == status);
+    }
+    if (search) {
+      tasks = tasks.filter(
+        (task) =>
+          task.title.includes(search) || task.description.includes(search),
+      );
+    }
+    return tasks;
   }
 
   createTask(createTaskDto: createTaskDto): Task {
@@ -26,4 +42,24 @@ export class TasksService {
   getTaskById(id: string): Task {
     return this.tasks.find((task) => task.id == id);
   }
+
+  deleteTaskById(id: string): Task {
+    const task = this.getTaskById(id);
+    this.tasks = this.tasks.filter((task) => task.id != id);
+    return task;
+  }
+
+  patchTaskById(id: string, type: string, value: string): Task {
+    const task = this.getTaskById(id);
+    if (type == 'status') {
+      task.status = TaskStatus[value.toUpperCase()];
+    } else if (type == 'title') {
+      task.title = value;
+    } else if (type == 'description') {
+      task.description = value;
+    }
+    return task;
+  }
 }
+
+// stores the actuall bussiness logic
