c71d32acdc1a32e5bb160a917e080b6632c11b5d
Pargat-Dhanjal <pargat14@hotmail.com>
2023-06-12 15:11:34 +0530

    feat: dto, get task by id

---
+++ src/tasks/dto/create-task.dto.ts
@@ -0,0 +1,6 @@
+export class createTaskDto {
+  title: string;
+  description: string;
+}
+
+// data tranfer object is passed around to avoid parameter mismatch
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import * as uuid from 'uuid';
+import { createTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
@@ -10,7 +11,8 @@ export class TasksService {
     return this.tasks;
   }
 
-  createTask(title: string, description: string): Task {
+  createTask(createTaskDto: createTaskDto): Task {
+    const { title, description } = createTaskDto;
     const task: Task = {
       id: uuid.v1(),
       title: title,
@@ -19,5 +21,9 @@ export class TasksService {
     };
     this.tasks.push(task);
     return task;
+  }
+
+  getTaskById(id: string): Task {
+    return this.tasks.find((task) => task.id == id);
   }
 }
